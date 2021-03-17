1908abaf430def031840762312c0a72b3fd8c1aa
Samuel Pond Marritz <sammypond@gmail.com>
2021-03-16 19:19:16 -0600

    Add files via upload

---
+++ fc_code.js
@@ -0,0 +1,163 @@
+<script>
+(function() {
+window._zi = {formId: 'PMux4gFV6sk4hPqi6COV'};
+var zi = document.createElement('script');
+zi.type = 'text/javascript';
+zi.async = true;
+zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(zi, s);
+})();
+</script>
+
+(function(){window._zi={formId:"U2sXL8cyYLl7Yx51k5y7,development: true"};var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src="https://ws-assets.zoominfo.com/formcomplete.js";var b=document.getElementsByTagName("script")[0];b.parentNode.insertBefore(a,b)})();
+
+(function() {
+    window._zi = {formId: 'ATBloEuociLDeaQpdr7U'};
+    var zi = document.createElement('script');
+    zi.type = 'text/javascript';
+    zi.async = true;
+    zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(zi, s);
+    })();
+
+
+
+setTimeout(function(){
+window._zi = { formId: 'Sfole8IFUliPI59FLnOe' };
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+},3000);
+
+
+setTimeout(function() {
+window._zi = {formId: 'Oxo9aafvmDiw64w9vote'};
+var zi = document.createElement('script');
+zi.type = 'text/javascript';
+zi.async = true;
+zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+var s = document.getElementsByTagName('script')[0];
+}, 6000);
+
+(function() {
+    window._zi = {formId: 'z0qkqwsrxXVw2XUOqZ1Y', development:true};
+    var zi = document.createElement('script');
+    zi.type = 'text/javascript';
+    zi.async = true;
+    zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(zi, s);
+    })();
+
+    (function() {
+        window._zi = {formId: 'z0qkqwsrxXVw2XUOqZ1Y', development:true};
+        var zi = document.createElement('script');
+        zi.type = 'text/javascript';
+        zi.async = true;
+        zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(zi, s);
+        })();
+
+        (function() {
+            window._zi = {formId: '8TBk42RGiNsqo4zjseps', development:true};
+            var zi = document.createElement('script');
+            zi.type = 'text/javascript';
+            zi.async = true;
+            zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(zi, s);
+            })();
+
+                (function() {
+                window._zi = {formId: '8TBk42RGiNsqo4zjseps'};
+                var zi = document.createElement('script');
+                zi.type = 'text/javascript';
+                zi.async = true;
+                zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(zi, s);
+                })();
+
+                (function() {
+                    window._zi = {formId: 'lHbDzzVZvT7SYNYhpkeq', development:true};
+                    var zi = document.createElement('script');
+                    zi.type = 'text/javascript';
+                    zi.async = true;
+                    zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+                    var s = document.getElementsByTagName('script')[0];
+                    s.parentNode.insertBefore(zi, s);
+                    })();
+
+                    
+form[data-zi-mapped-form]
+input​:not([type="email"]):not([type="submit"])  {​
+display​: none !important;
+}
+
+setTimeout(function(){window._zi={formId:"A32QRBDctik9MuL6cBpy"};var a=document.createElement("script");a.type="text/javascript";a.async=!0;a.src="https://ws-assets.zoominfo.com/formcomplete.js";document.getElementsByTagName("head")[0].appendChild(a)},1E3);
+
+
+//Just change the formId //
+setTimeout(function(){
+window._zi = { formId: 'Sfole8IFUliPI59FLnOe' };
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+document.getElementsByTagName('head')[0].appendChild(script);
+},3000);
+
+(function() {
+    window._zi = {formId: '2671DHRVC0qtMwQgrraR', development: true};
+    var zi = document.createElement('script');
+    zi.type = 'text/javascript';
+    zi.async = true;
+    zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(zi, s);
+    })();
+
+  /*  Here are the credentials to InboxAI Demo environments:
+Salesforce: username: sales@kdemo.co
+password: crush2021!
+O365: username: nancy@kdemo.co OR operation@kdemo.co
+password: crush2021!
+Note: there is no demo environment for GSuite */
+
+/* 9505515910611069758209 */
+
+(function () {
+window._zi = {formId: '7HTUJVpoOx1B3ZnCaQlo'};
+var zi = document.createElement('script');
+zi.type = 'text/javascript';
+zi.async = true;
+zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(zi, s);
+})();
+
+
+(function() {
+window._zi = {formId: '6Cb3XGiDLOadC9wRQvqX'};
+var zi = document.createElement('script');
+zi.type = 'text/javascript';
+zi.async = true;
+zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(zi, s);
+})();
+
+(function () {
+window._zi = {formId: 'KIACzUfSXNHfNLSFALOG'};
+var zi = document.createElement('script');
+zi.type = 'text/javascript';
+zi.async = true;
+zi.src = 'https://ws-assets.zoominfo.com/formcomplete.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(zi, s);
+})();
